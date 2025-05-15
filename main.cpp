70c2996b7a19668011f61aae32f198aaef8f35fc
zeqi <logosqi@163.com>
2025-05-15 17:48:26 +0800

    VS的国际化

--- main.cpp
+++ main.cpp
@@ -1,24 +1,53 @@
 ﻿#include "mainwindow.h"
 #include <QApplication>
 #include <QTranslator>
+#include <QDir>
+#include <QDebug>
+#include <QFile>
+
 #include "chart/shape.h"
 
+
+QString getQmlPath()
+{
+    //这里为调试目录
+    QString appDir = QCoreApplication::applicationDirPath();
+    QDir currentDir(appDir);
+
+    QString qmlFilePath = QDir::cleanPath(currentDir.absolutePath() + QDir::separator() +
+        QString("mui") + QDir::separator() +
+        QString("zh_CN") + QDir::separator() +
+        QString("lessoncode.qm")
+    );
+
+    if (QFile::exists(qmlFilePath))
+    {
+        return qmlFilePath;
+    }
+
+    return QString();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     
-    // Set up translator
     QTranslator translator;
-    // Load translations based on system locale
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "StepByStepFlowChart_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
+    if (translator.load(getQmlPath()))
+    {
+        a.installTranslator(&translator);
     }
+    // // Set up translator
+    // QTranslator translator;
+    // // Load translations based on system locale
+    // const QStringList uiLanguages = QLocale::system().uiLanguages();
+    // for (const QString &locale : uiLanguages) {
+    //     const QString baseName = "StepByStepFlowChart_" + QLocale(locale).name();
+    //     if (translator.load(":/i18n/" + baseName)) {
+    //         a.installTranslator(&translator);
+    //         break;
+    //     }
+    // }
     
     MainWindow w;
     w.show();
