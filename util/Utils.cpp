5483204e4d9ddce1e7e464a3b34029944d572f4d
zeqi <logosqi@163.com>
2025-05-15 09:35:53 +0800

    微调尺寸

--- util/Utils.cpp
+++ util/Utils.cpp
@@ -1,24 +1,7 @@
 #include "util/Utils.h"
 #include <QPainter>  // 添加QPainter的头文件
 
-// 定义默认尺寸（像素）
-static constexpr int Default_WIDTH = 1600;    
-static constexpr int Default_HEIGHT = 780;   
 
-// 定义A4尺寸（像素）
-static constexpr int A4_WIDTH = 1050;    // A4宽度为1050像素
-static constexpr int A4_HEIGHT = 1500;   // A4高度为1500像素
-
-// 定义A3尺寸（像素）
-static constexpr int A3_WIDTH = 1500;    // A3宽度为1500像素
-static constexpr int A3_HEIGHT = 2100;   // A3高度为2100像素
-
-// 定义A5尺寸（像素）
-static constexpr int A5_WIDTH = 750;    // A5宽度为750像素
-static constexpr int A5_HEIGHT = 1050;   // A5高度为1050像素
-
-static constexpr qreal MAX_SCALE = 2.0;      // 最大缩放比例
-static constexpr qreal MIN_SCALE = 0.20;     // 最小缩放比例
 
 QCursor Utils::getCrossCursor(){
     QPixmap pixmap(32, 32);
--- util/Utils.h
+++ util/Utils.h
@@ -20,19 +20,19 @@ public:
     // 静态工具方法
     static QCursor getCrossCursor();
 
-    static constexpr int Default_WIDTH = 1600;    
-    static constexpr int Default_HEIGHT = 840;
+    static const int Default_WIDTH = 1600;    
+    static const int Default_HEIGHT = 780;
         // 定义A4尺寸（像素）
-    static constexpr int A4_WIDTH = 1050;    // A4宽度为1050像素
-    static constexpr int A4_HEIGHT = 1500;   // A4高度为1500像素
+    static const int A4_WIDTH = 1050;    // A4宽度为1050像素
+    static const int A4_HEIGHT = 1500;   // A4高度为1500像素
 
     // 定义A3尺寸（像素）
-    static constexpr int A3_WIDTH = 1500;    // A3宽度为1500像素
-    static constexpr int A3_HEIGHT = 2100;   // A3高度为2100像素
+    static const int A3_WIDTH = 1500;    // A3宽度为1500像素
+    static const int A3_HEIGHT = 2100;   // A3高度为2100像素
 
     // 定义A5尺寸（像素）
-    static constexpr int A5_WIDTH = 750;    // A5宽度为750像素
-    static constexpr int A5_HEIGHT = 1050;   // A5高度为1050像素
+    static const int A5_WIDTH = 750;    // A5宽度为750像素
+    static const int A5_HEIGHT = 1050;   // A5高度为1050像素
 
     static constexpr qreal MAX_SCALE = 2.0;      // 最大缩放比例
     static constexpr qreal MIN_SCALE = 0.35;     // 最小缩放比例
